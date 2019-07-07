17208f8b7daa67fbca0fda1775ece9bb9860a9c4
Michal Terranova <michalterranova@mail.com>
2019-07-06 21:04:39 -0400

    Working game minus reset button and css needs work

---
+++ assets/javascript/app.js
@@ -0,0 +1,126 @@
+
+    var questions = [
+        {
+            question: "What is the name for the legendary shootout in which 8 lawmen and outlaws fought one another in Tombstone, Arizona Territory?", 
+            choices: ["Kansas City massacre", "Frisco shootout", "Goingsnake massacre", "OK Corral"],
+            goodAnswer: "OK Corral"
+         },
+         {
+             question: "Which of the following was NOT another title for ‘cowboy’?", 
+            choices: ["Cowhands", "Cowpunchers", "Cowkickers", "Buckaroos"],
+            goodAnswer: "Cowkickers"
+         }, 
+         {
+             question: "Who did the cowboys consider as the most important person in camp?", 
+            choices: ["The Cook", "The Wrangler", "The Women Folk", "The Trail Boss"],
+            goodAnswer: "The Cook"
+        }, 
+        {
+            question: "In the old west, whiskey sold at the saloons was made up of burnt sugar, alcohol, and what other ingredient?", 
+            choices: ["Tea Leaves", "Cactus Juice","Chewing Tobacco", "Flour"],
+            goodAnswer: "Chewing Tobacco"
+        }, 
+        {
+            question: "What did the saloon girls typically carry on them for self-defense?", 
+            choices: ["Pistols", "Perfumes", "Whiskey Bottles", "Whistles"],
+            goodAnswer: "Pistols"
+        }, 
+        {
+            question: "Which animal is not native to the American West that populated there due to European settlers?", 
+            choices: ["Wild Horses", "Road-Runners", "Rattle Snakes", "Flying-Squirrels"],
+            goodAnswer: "Wild Horses"
+        }, 
+        {
+            question: "Alice Ivers, became known as ‘Poker Alice’ after moving West in 1865 and becoming quite well known for her skills in poker. Where was she born originally?", 
+            choices: ["Virginia", "England", "Mexico", "Ohio" ],
+            goodAnswer: "England"
+        }, 
+        {
+            question: "Wild Bill Hickock was amongst the legendary gunfighters in the West. Legend has it that because of his love for the game of poker he died with what 4 cards in his hands which became known as the ‘Dead Man’s Hand’?", 
+            choices: ["A pair of jacks and a pair of 10s", "A pair of kings, a jack, and a two", "A pair of 10s and a pair of aces", "A pair of eights and a pair of aces" ],
+            goodAnswer: "A pair of eights and a pair of aces"
+        }];
+
+$('#startGame').on('click', function(){
+  game.start();
+});
+
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 90,
+  // Setting my counter
+  countDown: function(){
+    game.counter--;
+    $('#counter').html(game.counter)
+    if(game.counter <= 0){
+      game.completed();
+    }
+    if($('#complete').on('click', function(){
+      game.completed();
+    }));
+  },
+  // start the game with a timer and add all the questions and answers
+  start: function(){
+    timer = setInterval(game.countDown, 1000)
+    $('#questionaire').prepend('<h2 id="secondsRemaining" >Time Remaining: <span id="counter">90</span> Seconds</h2>');
+    $('#startGame, .game').remove();
+    for(var i=0; i<questions.length; i++){
+      $('#questionaire').append('<br><h3>' + questions[i].question + '</h3>')
+      for(var j=0; j < questions[i].choices.length; j++){
+        $('#questionaire').append("<div class ='userInput'><input class='button' type='radio' name='q"+ i +"' value='"+questions[i].choices[j]+"'><label class='label'>"+ questions[i].choices[j]+"  </label></div>");
+      }
+    }
+    $('<br><br><button id="complete" class="completed-btn" type="button" name="button-completed">Finished Quiz</button>').appendTo('#questionaire');
+  },
+  // checking each input if it is a correct answer or wrong answer and incrementing my game.correct and game.incorrect
+  completed: function(){
+
+    for (var i = 0; i < questions.length; i++) {
+      $.each($('input[name="question-'+ i +'"]:checked'), function(){
+        if($(this).val() === questions[i].goodAnswer){
+          game.correct++;
+        }
+        else {
+          game.incorrect++;
+        }
+      });
+    //     function testEnd(timer) {
+    //       if (timer <= 10000) {
+    //         $('#counter').style.color = 'red';
+    //     }
+    // }
+  }
+    // Calculate results
+    this.result();
+  },
+  // clear the timer
+  result: function(){
+    clearInterval(timer);
+    $('#questionaire h2').remove();
+
+    if (game.correct >= 6){
+      $('#questionaire').html('<h2>Looks like there\'s a new sheriff in town!</h2> <br> <img src="assets/images/q8.gif" alt="high"><br>').addClass('finished');
+    }
+    else if (game.correct >=4){
+      $('#questionaire').html('<h2>Better Luck Next Time, Cowboy!</h2> <br> <img src="assets/images/q2.gif" alt="mid"><br>').addClass('finished');
+    }
+    else {
+      $('#questionaire').html('<h2>Best be leavin\' til ya know what you\'re doin\'!<br> <img src="assets/images/q1.gif" alt="low"><br>').addClass('finished');
+    }
+    $('#questionaire').append("<h3>Questions Ya Done Well: "+this.correct+"</h3>");
+    $('#questionaire').append("<h3>Questions Ya Ain't Done Well: "+this.incorrect+"</h3>");
+    // display all the Unanswered questions
+    $('#questionaire').append("<h3>Questions You Didn't Round Up: "+(questions.length-(this.incorrect+this.correct))+"<h3>");
+    $('#questionaire').append('<br><button id="restart" class="restart-btn" type="button" name="button-restart">Do it Again?</button>').appendTo('#questionaire');
+
+    
+$('#restart').on('click', function(){
+  game.start();
+});
+  },
+}
+
+$('#completedGame').on('click', function(){
+  game.countDown();
+});
